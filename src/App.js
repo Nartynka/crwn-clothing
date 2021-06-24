45df048809f1ffd4cc06e5f0d196455fce5fddfe
Martyna <mplutwska@gmail.com>
2021-06-24 15:49:23 +0200

    Added react-router-dom, updated menu-item to navigate withRouter higher-order component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+
+import { Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () => <h1>HATS</h1>;
 
 function App() {
 	return (
 		<div className='App'>
-			<HomePage />
+			<Route exact path='/' component={HomePage} />
+			<Route exact path='/hats' component={HatsPage} />
 		</div>
 	);
 }
